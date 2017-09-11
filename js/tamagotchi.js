ef4536fd8d45c35466b27edae23ff43f332d78a3
Jessica Sheridan and Trevor Gill <jessica@>
2017-09-11 16:45:45 -0700

    additional giveFood functionality

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -1,110 +1,56 @@
-export let pet {
-  food: 8,
-  setFood: function() {
-    const foodInterval = setInterval(() =>{
-      this.food--;
-      if (this.isDead() == true) {
-        clearInterval(foodInterval);
-        return "Your tamagotchi is dead!";
-      }
-    }, 1000);
-  },
-  isDead: function() {
-    if (this.food > 0) {
-      return false;
-    } else {
-      return true;
-    }
-  },
-  feed: function(amount) {
-    let that = this;
-    return function(food) {
-      that.food += amount;
-      return that.name + `ate the ${food} you gave it! It's food level is now ${that.food}`;
-    }
+export class Tamagotchi {
+  constructor(name, rest, food, love) {
+    this.name = name;
+    this.rest = rest;
+    this.food = food;
+    this.love = love;
   }
-};
-pet.eatSmall = pet.feed(2);
 
 
-//
-//   constructor(name, rest, food, love) {
-//     this.name = name;
-//     this.rest = rest;
-//     this.food = food;
-//     this.love = love;
-//   }
-//
-//   setRest() {
-//     setInterval(() => {
-//       this.rest--;
-//     }, 1000);
-//   }
-//
-//   setFood() {
-//     setInterval(() => {
-//       this.food--;
-//     }, 1000);
-//   }
-//
-//   setLove() {
-//     setInterval(() => {
-//       this.love--;
-//     }, 1000);
-//   }
-//
-//   giveRest() {
-//     this.rest += 10;
-//   }
-//
-//   giveLove() {
-//     this.love += 2.5;
-//   }
-//
-//   giveFood() {
-//     this.food += 4;
-//   }
-//
-// }
+  setFood() {
+    setInterval(() => {
+      this.food--;
+    }, 1000);
+  }
+  // setRest() {
+  //   setInterval(() => {
+  //     this.rest--;
+  //   }, 1000);
+  // }
+  // setLove() {
+  //   setInterval(() => {
+  //     this.love--;
+  //   }, 1000);
+  // }
+  //
+  // giveRest() {
+  //   this.rest += 10;
+  // }
+  //
+  // giveLove() {
+  //   this.love += 2.5;
+  // }
+  getFoodValues(type) {
+    const menuMap = {};
+    menuMap["blueberries"] = 2;
+    menuMap["pb&j"] = 4;
+    menuMap["lasagna"] = 6;
+    return menuMap[type];
+  }
 
+  giveFood(type) {
+    this.food += this.getFoodValues(type);
+    return `Yum, mighty tasty ${type}! ${this.name} is pleased; current food level: ${this.food}`;
+  }
 
-// export class Tamagotchi {
-//
-//   constructor(name, rest, food, love) {
-//     this.name = name;
-//     this.rest = rest;
-//     this.food = food;
-//     this.love = love;
-//   }
-//
-//   setRest() {
-//     setInterval(() => {
-//       this.rest--;
-//     }, 1000);
-//   }
-//
-//   setFood() {
-//     setInterval(() => {
-//       this.food--;
-//     }, 1000);
-//   }
-//
-//   setLove() {
-//     setInterval(() => {
-//       this.love--;
-//     }, 1000);
-//   }
-//
-//   giveRest() {
-//     this.rest += 10;
-//   }
-//
-//   giveLove() {
-//     this.love += 2.5;
-//   }
-//
-//   giveFood() {
-//     this.food += 4;
-//   }
-//
-// }
+  isDead() {
+    let dead;
+    if(this.food <= 0 || this.rest <= 0 || this.love <= 0){
+      dead = true;
+    } else {
+      dead = false;
+    }
+    return dead;
+  }
+
+}
--- spec/tamagotchi-spec.js
+++ spec/tamagotchi-spec.js
@@ -1,49 +1,58 @@
-import { pet } from './../js/tamagotchi.js';
+import { Tamagotchi } from './../js/tamagotchi.js';
 
 describe ('Tamagotchi' , function() {
-  // let pet = new Tamagotchi("testPet", 20, 8, 5);
-  let testPet = pet;
+  let testPet = new Tamagotchi("testPet", 20, 8, 5);
 
   beforeEach(function() {
     jasmine.clock().install();
     testPet.rest = 20;
     testPet.food = 8;
     testPet.love = 5;
-    testPet.setRest();
+    // testPet.setRest();
     testPet.setFood();
-    testPet.setLove();
+    // testPet.setLove();
   });
 
   afterEach(function() {
     jasmine.clock().uninstall();
   });
 
-  it('should have a name and a rest level of 20 when it is created', function() {
-    expect(pet.name).toEqual("testPet");
-    expect(pet.rest).toEqual(20);
-  });
+  // it('should have a name and a rest level of 20 when it is created', function() {
+  //   expect(pet.name).toEqual("testPet");
+  //   expect(pet.rest).toEqual(20);
+  // });
 
   it('should have a food level of 8 when it is created', function() {
-    expect(pet.food).toEqual(8);
+    expect(testPet.food).toEqual(8);
   });
 
-  it('should have a love level of 5 when it is created', function() {
-    expect(pet.love).toEqual(5);
-  });
-
-  it('should have a rest level of 17 after 3001 milliseconds', function() {
-    jasmine.clock().tick(3001);
-    expect(pet.rest).toEqual(17);
-  });
+  // it('should have a love level of 5 when it is created', function() {
+  //   expect(pet.love).toEqual(5);
+  // });
+  //
+  // it('should have a rest level of 17 after 3001 milliseconds', function() {
+  //   jasmine.clock().tick(3001);
+  //   expect(pet.rest).toEqual(17);
+  // });
 
   it('should have a food level of 5 after 3001 milliseconds', function() {
     jasmine.clock().tick(3001);
-    expect(pet.food).toEqual(5);
+    expect(testPet.food).toEqual(5);
   });
 
-  it('should have a love level of 2 after 3001 milliseconds', function() {
-    jasmine.clock().tick(3001);
-    expect(pet.love).toEqual(2);
+  it('should increase food after giveFood', function() {
+    expect(testPet.giveFood("blueberries")).toEqual('Yum, mighty tasty blueberries! testPet is pleased; current food level: 10');
   });
+
+  it('should kill tamagotchi when food <= 0', function() {
+    jasmine.clock().tick(8001);
+    expect(testPet.isDead()).toEqual(true);
+  });
+
+
+  // it('should have a love level of 2 after 3001 milliseconds', function() {
+  //   jasmine.clock().tick(3001);
+  //   expect(pet.love).toEqual(2);
+  // });
 
 });
